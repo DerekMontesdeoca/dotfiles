399a0ddeb0767a2b705174437a699e3cf26b5acf
Derek Montesdeoca <demont93@gmail.com>
2025-03-26 23:36:17 +0100

    Name files explicitly in the install script.

--- install.sh
+++ install.sh
@@ -4,13 +4,9 @@ set -euo pipefail
 
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
-if ! command -v bash-it; then
-    git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-    ~/.bash_it/install.sh
-fi
 if ! command -v fzf; then
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install
 fi
-rsync -av ./ "${HOME}"
-bash-it profile load demont93
+rsync -azv "$SCRIPT_DIR/.config" "$SCRIPT_DIR/.local" "$SCRIPT_DIR/.bashrc" \
+    "$SCRIPT_DIR/.profile" "$HOME"
